75ec6bf5afe6b6f2a92e005d010260cbf43c2d34
Nithusikan001 <e19266@eng.pdn.ac.lk>
2025-06-22 17:31:01 +0530

    feat: :art: Chroma store code file uploaded.

---
+++ vectorstore/chroma_store.py
@@ -0,0 +1,8 @@
+from langchain.vectorstores import Chroma
+from embeddings.embedder import get_embedding_model
+
+def get_vectorstore(documents, persist_directory="chroma_db"):
+    embedding_model = get_embedding_model()
+    vectordb = Chroma.from_documents(documents, embedding_model, persist_directory=persist_directory)
+    vectordb.persist()
+    return vectordb

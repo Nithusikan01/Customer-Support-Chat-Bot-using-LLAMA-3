56c2236281ad18f7f8e56a0aa1588b2e213d0c4a
Nithusikan001 <e19266@eng.pdn.ac.lk>
2025-10-08 09:27:50 +0530

    feat: :zap: Bug fixed

--- llm/llama_model.py
+++ llm/llama_model.py
@@ -8,8 +8,18 @@ load_dotenv()
 def load_llama_model():
     model_name = os.getenv("MODEL_NAME")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype="auto")
     
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=512)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, 
+        device_map="auto", 
+        torch_dtype="auto"
+        )
+    
+    pipe = pipeline(
+        "text-generation", 
+        model=model, 
+        tokenizer=tokenizer, 
+        max_new_tokens=512
+        )
     llm = HuggingFacePipeline(pipeline=pipe)
     return llm
--- vectorstore/chroma_store.py
+++ vectorstore/chroma_store.py
@@ -3,6 +3,10 @@ from embeddings.embedder import get_embedding_model
 
 def get_vectorstore(documents, persist_directory="chroma_db"):
     embedding_model = get_embedding_model()
-    vectordb = Chroma.from_documents(documents, embedding_model, persist_directory=persist_directory)
+    vectordb = Chroma.from_documents(
+        documents, 
+        embedding_model, 
+        persist_directory=persist_directory
+        )
     vectordb.persist()
     return vectordb
